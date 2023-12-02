a0b943329d6a4c5f867e8366762f2689bb4e553a
fayyra <kfiruz193@gmail.com>
2023-12-03 00:46:01 +0500

    few more

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,8 +1,10 @@
 package com.epam.utils;
 
 
+import static java.util.Objects.isNull;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        return !str.isEmpty() && !str.startsWith("-") && !str.equals("0");
+        return !isNull(str) && !str.isEmpty() && !str.startsWith("-") && !str.equals("0");
     }
 }
