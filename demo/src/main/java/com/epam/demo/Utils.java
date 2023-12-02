4eebbfc11e711b5b190b0af14dd36391aff207ab
fayyra <kfiruz193@gmail.com>
2023-12-03 00:29:43 +0500

    third

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
         for (String s: args) {
-            if (s.isEmpty() || Integer.parseInt(s) < 0)
+            if (s.isEmpty() || s.startsWith("-"))
                 return false;
         }
         return true;
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,8 +2,6 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        if (!str.isEmpty())
-            return Integer.parseInt(str) > 0;
-        return false;
+        return !str.isEmpty() && !str.startsWith("-");
     }
 }
